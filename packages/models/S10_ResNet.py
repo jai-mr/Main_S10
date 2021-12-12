9dbddaf31f0c3ceac7755409ea998c3f89542470
jai-mr <68414965+jai-mr@users.noreply.github.com>
2021-12-12 21:41:00 +0530

    Add files via upload

--- packages/models/S10_ResNet.py
+++ packages/models/S10_ResNet.py
@@ -38,9 +38,8 @@ class BasicBlock(nn.Module):
         out = F.relu(out)
         return out
 
-
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=200):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
@@ -50,7 +49,7 @@ class ResNet(nn.Module):
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=1)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
@@ -73,9 +72,17 @@ class ResNet(nn.Module):
         return out
 
 
-def CusResNet18():
+def S12_ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
 
 
-def CusResNet34():
+def S12_ResNet34():
     return ResNet(BasicBlock, [3, 4, 6, 3])
+
+
+def test():
+    net = ResNet18_S12()
+    y = net(torch.rand(1, 3, 32, 32))
+    print(y.size())
+
+#test()
